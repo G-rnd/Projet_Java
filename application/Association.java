fb9e5bb8c9e011bcbf215fb5734a35ed4597ce71
G-rnd <76098938+G-rnd@users.noreply.github.com>
2021-05-05 16:46:09 +0200

    Update Association.java

--- application/Association.java
+++ application/Association.java
@@ -1,6 +1,7 @@
 package application;
 
 import java.util.LinkedList;
+import java.util.Scanner;
 
 public class Association {
     private LinkedList<ArbreAssociation> listeArbres;
@@ -21,6 +22,8 @@ public class Association {
                 for(int i = 0; i < listeArbres.size(); i++) {
                     // cherche un arbre non déjà choisi.
                     if (listeProposee.contains(listeArbres.get(i).getArbre())) { continue; }
+                    // s'il est déjà remaquable, on ne le comptabilise pas.
+                    if (listeArbres.get(i).isEstRemarquable()) { continue; }
                     else {
                         if (init) {
                             // choix entre deux arbres : le max temporaire et le 1-eme arbre de la liste.
@@ -57,9 +60,94 @@ public class Association {
         }
         else {
             for(int i = 0; i < listeArbres.size(); i++) {
-                listeProposee.add(listeArbres.get(i).getArbre());
+                if (listeArbres.get(i).isEstRemarquable()) { continue; }
+                else {
+                    listeProposee.add(listeArbres.get(i).getArbre());
+                }
             }
         }
         return listeProposee;
+    }
+
+    public boolean estInscrit(Personne p) {
+        for(int i = 0; i < this.listeMembres.size(); i++) {
+            if(this.listeMembres.get(i).getPersonne().equals(p)) { return true;}
+        }
+        return false;
+    }
+
+    public void inscrire(Personne p, int[] date, double cotisationEntree) {
+        StringBuilder error = new StringBuilder("Erreur inscription membre : ");
+        boolean res = true;
+        try {
+            if (this.estInscrit(p)) { throw new ExceptionInInitializerError("Personne déjà inscrite."); }
+            if (dateValide.estValide(date)) {
+                if (cotisationEntree < 0) {
+                    error.append(", Cotisation d'inscription invalide.");
+                    res = false;
+                }
+            }
+            else {
+                res = false;
+                error.append("Date invalide");
+                if (cotisationEntree <= 0) {
+                    error.append(", Cotisation d'inscription invalide");
+                }
+                error.append(".");
+            }
+            if (res) { this.listeMembres.add(new Membre(p, date, cotisationEntree)); }
+            else { throw new ExceptionInInitializerError(error.toString()); }
+        }
+        catch (ExceptionInInitializerError e) {
+            System.out.println(e.toString());
+        }
+    }
+
+    public void voteMembre(Membre m, ArbreAssociation a) {
+        if(m.getNbVotes() > 0) {
+            if(!a.isEstRemarquable()) {
+                m.listeArbresVotes.add(a);
+                m.voterArbre(a);
+            }
+            else { System.out.println("Vote : arbre déjà remarquable."); }
+        }
+        else {
+            m.listeArbresVotes.remove(0);
+            m.listeArbresVotes.add(a);
+            System.out.println("Vote : Le plus ancien choix a été retiré.");
+        }
+    }
+
+    public void exclure(Membre m) {
+        // TODO avec les CR.
+        for(int i = 0; i < listeArbres.size(); i++) {
+            listeArbres.get(i).masquer(m);
+        }
+        // retirer les votes
+        if(m.getNbVotes() < 5) {
+            for(int i = 0; i < m.listeArbresVotes.size(); i++) {
+                m.listeArbresVotes.get(i).removeNbVotes();
+            }
+        }
+
+        // si besoin de retirer autre choses.
+        m = null;
+    }
+
+
+    public static void main(String[] args) {
+        boolean ok = true;
+        try ( Scanner scanner = new Scanner( System.in ) ) {
+            while(ok) {
+                // affichage interface choix.
+                System.out.println( "Veuillez saisir une action.");
+                int res = scanner.nextInt();
+
+                System.out.print("lu : ");
+                System.out.println(res);
+                if(res == -1) { ok = false; }
+
+            }
+        }
     }
 }
