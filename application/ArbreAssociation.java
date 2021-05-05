77998879fef37b4868b807bc7c104d3aa9b36f2f
G-rnd <76098938+G-rnd@users.noreply.github.com>
2021-05-05 16:46:30 +0200

    Update ArbreAssociation.java

--- application/ArbreAssociation.java
+++ application/ArbreAssociation.java
@@ -3,13 +3,19 @@ package application;
 import java.util.LinkedList;
 
 public class ArbreAssociation {
-    Arbre arbre;
+    private Arbre arbre;
 
+    private int nbVotes = 0;
     private boolean estRemarquable;
     private int[] dateRemarquable = new int[] {0, 0, 0};
     // à adapter en LinkedList<CR>
     private LinkedList<String> listeCR;
 
+    protected boolean isEstRemarquable() { return this.estRemarquable; }
+    protected int getNbVotes() { return this.nbVotes; }
+    protected void addNbVotes() { this.nbVotes++; }
+    protected void removeNbVotes() { this.nbVotes--; }
+    protected Arbre getArbre() { return this.arbre; }
     public ArbreAssociation(Arbre a) {
         this.arbre = a;
     }
@@ -38,11 +44,16 @@ public class ArbreAssociation {
             System.out.println("L'arbre n'a pas pu être rendu remarquable.");
         }
     }
+    // TODO masquer = remplacer celui qui a écrit + celui qui a organisé par << personne ayant quitté l'association >>.
+    protected void masquer(Membre m) {
+
+    }
+
 
     public static void main(String[] args) {
         Arbre a1 = new Arbre("bla","bla2","blabla",1,1.1,"grand",
                 "rue", new double[] {1.0,1.0});
         ArbreAssociation aRem = new ArbreAssociation(a1);
-        aRem.rendreRemarquable(new int[] {5,5, 2021});
+        aRem.rendreRemarquable(new int[] {5,9, 2021});
     }
 }
