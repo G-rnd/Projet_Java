de74d4992acf5c9d7d9fd7395f350d8023ff0772
G-rnd <76098938+G-rnd@users.noreply.github.com>
2021-05-05 16:47:14 +0200

    Update Membre.java

--- application/Membre.java
+++ application/Membre.java
@@ -8,37 +8,22 @@ public class Membre {
     private int[] dateInscription;
     // à adapter en LinkedList<Cotisations> si besoin
     private LinkedList<Double> listeCotisations = new LinkedList<Double>();
+    protected LinkedList<ArbreAssociation> listeArbresVotes = new LinkedList<ArbreAssociation>();
 
-    protected LinkedList<Arbre> listeArbresVotes;
     private int nombreVotes;
 
+    protected void voterArbre(ArbreAssociation a) {
+        this.nombreVotes--;
+        this.listeArbresVotes.add(a);
+        a.addNbVotes();
+    }
+    protected int getNbVotes() { return this.nombreVotes; }
+    protected Personne getPersonne() { return this.p; }
+
     protected Membre(Personne p, int[] dateInscription, double cotisationEntree) {
-        StringBuilder error = new StringBuilder("Erreur inscription membre : ");
-        boolean res = true;
-        try {
-            if (dateValide.estValide(dateInscription)) {
-                this.p = p;
-                this.dateInscription = dateInscription;
-                if (cotisationEntree > 0) {
-                    this.listeCotisations.add(cotisationEntree);
-                }
-                else {
-                    error.append(", Cotisation d'inscription invalide.");
-                    res = false;
-                }
-            }
-            else {
-                res = false;
-                error.append("Date invalide");
-                if (cotisationEntree <= 0) {
-                    error.append(", Cotisation d'inscription invalide");
-                }
-                error.append(".");
-            }
-            if (!res) { throw new ExceptionInInitializerError(error.toString()); }
-        }
-        catch (ExceptionInInitializerError e) {
-            System.out.println(e.toString());
-        }
+        this.p = p;
+        this.dateInscription = dateInscription;
+        this.listeCotisations.add(cotisationEntree);
+        this.nombreVotes = 5;
     }
 }
