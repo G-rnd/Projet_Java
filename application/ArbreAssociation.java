87c1d6f6c245838e3c0a77d1ef2ad6a89ce8990a
G-rnd <76098938+G-rnd@users.noreply.github.com>
2021-05-06 12:53:42 +0200

    Update ArbreAssociation.java

--- application/ArbreAssociation.java
+++ application/ArbreAssociation.java
@@ -8,26 +8,51 @@ public class ArbreAssociation {
     private int nbVotes = 0;
     private boolean estRemarquable;
     private int[] dateRemarquable = new int[] {0, 0, 0};
-    // à adapter en LinkedList<CR>
-    private LinkedList<String> listeCR;
+    // TODO à adapter en LinkedList<CR>
+    protected LinkedList<String> listeCR;
+
+    private int[] dateDerniereVisite = new int[] {0, 0, 0};
+    protected int[] getDateRemarquable() {
+        return this.dateRemarquable.clone();
+    }
+    protected void setDateDerniereVisite(int[] d) {
+        try {
+            if (dateValide.estValide(d)) {
+                this.dateDerniereVisite = d;
+            }
+            else {
+                throw new ExceptionInInitializerError("Date invalide");
+            }
+        }
+        catch (ExceptionInInitializerError e) {
+            System.out.println(e.toString());
+        }
+    }
+
 
     protected boolean isEstRemarquable() { return this.estRemarquable; }
     protected int getNbVotes() { return this.nbVotes; }
     protected void addNbVotes() { this.nbVotes++; }
     protected void removeNbVotes() { this.nbVotes--; }
     protected Arbre getArbre() { return this.arbre; }
-    public ArbreAssociation(Arbre a) {
-        this.arbre = a;
+
+    //déclarer un arbre de l'association
+    public ArbreAssociation(Arbre a) { this.arbre = a; }
+    // déclarer un arbre de l'association en disant s'il est remarquable ou non
+    public ArbreAssociation(Arbre a, boolean estRemarquable) {
+        this(a);
+        this.estRemarquable = estRemarquable;
     }
-    public ArbreAssociation(Arbre a, boolean estRemarquable, int[] dateRemarquable) {
+    // déclarer un arbre de l'association remarquable en lui donnant une date de classification
+    public ArbreAssociation(Arbre a, int[] dateRemarquable) {
         this(a);
         try {
-            if (estRemarquable == true) {
-                this.estRemarquable = estRemarquable;
+            if (dateValide.estValide(dateRemarquable)) {
+                this.estRemarquable = true;
                 this.rendreRemarquable(dateRemarquable);
             }
             else {
-                throw new ExceptionInInitializerError();
+                throw new ExceptionInInitializerError("Erreur : Date incorrecte.");
             }
         }
         catch (ExceptionInInitializerError e) {
@@ -54,6 +79,6 @@ public class ArbreAssociation {
         Arbre a1 = new Arbre("bla","bla2","blabla",1,1.1,"grand",
                 "rue", new double[] {1.0,1.0});
         ArbreAssociation aRem = new ArbreAssociation(a1);
-        aRem.rendreRemarquable(new int[] {5,9, 2021});
+        aRem.rendreRemarquable(new int[] {5, 5, 2021});
     }
 }
