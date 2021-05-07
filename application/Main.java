635908b5ccd28b06ff9a5a2dd587e537656f8c80
G-rnd <76098938+G-rnd@users.noreply.github.com>
2021-05-07 22:16:53 +0200

    Update Main.java

--- application/Main.java
+++ application/Main.java
@@ -1,8 +1,50 @@
 package application;
 
+import data.FileReader;
+
+import java.io.File;
+import java.util.ArrayList;
+
 public class Main
 {
     public static void main(String[] args) {
-        Association.main(args);
+        // techniquement déclarer dans espaces verts
+        ArrayList<Arbre> listeArbres = null;
+        if(args.length > 0)
+        {
+            File tempFile = new File(args[0]);
+            if(tempFile.exists())
+            {
+                System.out.println("[Main] : Lecture du fichier " + args[0] + " ...");
+
+                //We start by reading the CSV file
+                listeArbres = FileReader.getDataFromCSVFile(args[0]);
+
+                System.out.println("[Main] : Fin du fichier " + args[0] + ".");
+            }
+            else { System.out.println("[Main] Aucun fichier trouvé."); }
+        }
+        else { System.out.println("[Main] Aucun argument trouvé."); }
+
+        if(listeArbres != null) {
+            Association AssociationArbre = new Association(listeArbres);
+            // l'argument 2 est pour le debug
+            try {
+                if (args[1].equals("test")) {
+                    Test.main(AssociationArbre);
+                }
+            }
+            catch (Exception e) { /* pas de debug. */ }
+            try {
+                if (!args[2].equals("only")) {
+                    // lancement de l'application.
+                    Application.main(AssociationArbre);
+                }
+            }
+            catch (Exception e) {
+                Application.main(AssociationArbre);
+            }
+        }
+        else { System.out.println("[Main] : Aucun Arbre n'a été lu."); }
     }
 }
