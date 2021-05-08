f827555c179d8be85e7e511fdc7d091238d124b8
G-rnd <76098938+G-rnd@users.noreply.github.com>
2021-05-08 19:09:33 +0200

    Update Membre.java

--- application/Membre.java
+++ application/Membre.java
@@ -3,36 +3,80 @@ package application;
 import java.util.Collections;
 import java.util.LinkedList;
 
+/**
+ * Membre.
+ */
 public class Membre {
-    private Personne p;
+    // Attributs
+    private final Personne p;
     private boolean president;
     private boolean cotisation;
-    private int nbVisites;
-
-    public final int nbVotesMax = 5;
-    protected final static int nbVisitesMax = 20;
-
-    private int[] dateInscription;
-    protected LinkedList<ArbreAssociation> listeArbresVotes = new LinkedList<>();
 
     private int nbVotes;
+    public final static int nbVotesMax = 5;
 
+    private int nbVisites;
+    protected final static int nbVisitesMax = 20;
+
+    private final int[] dateInscription;
+    protected LinkedList<ArbreAssociation> listeArbresVotes = new LinkedList<>();
+
+    // Méthodes
+
+    /**
+     * Renvoie le nombre de visites disponibles pour le membre.
+     * @return L'attribut nbVisites.
+     */
     public int getNbVisitesRestantes() { return this.nbVisites; }
+
+    /**
+     * Retire une visite au nombre de visites disponibles.
+     */
     protected void removeNbVisites() {
         if (this.getNbVisitesRestantes() > 0) { this.nbVisites--; }
     }
+
+    /**
+     * Met le nombre de visites restantes d'un membre à jour.
+     * @param n Le nombre de visites restantes.
+     */
     protected void setNbVisites(int n) {
         if(n < nbVisitesMax && n >= 0) { this.nbVisites = n; }
-        else { System.out.println("setNbVisites : Le nombre de visites saisi est incorrecte."); }
+        else { System.out.println("[Membre] : Le nombre de visites saisi est incorrecte."); }
     }
 
+    /**
+     * Renvoie un booléen à VRAI si le membre est président, à FAUX sinon.
+     * @return L'attribut president du membre.
+     */
     public boolean isPresident() { return  this.president; }
 
+    /**
+     * Met l'attribut president à VRAI.
+     */
     public void setPresident() { this.president = true; }
+
+    /**
+     * Met l'attribut president à FAUX.
+     */
     public void resetPresident() { this.president = false; }
 
+    /**
+     * Permet de savoir si le membre a payé sa cotisation annuelle.
+     * @return L'attribut cotisation du membre.
+     */
     public boolean isCotisation() { return this.cotisation; }
+
+    /**
+     * Met à jour l'attribut cotisation du membre.
+     * @param b La valeur nouvelle.
+     */
     public void setCotisation(boolean b) { this.cotisation = b; }
+
+    /**
+     * Réalise le paiement de la cotisation annuelle d'un membre.
+     * @return Un booléen à VRAI si le paiement a réussi, à FAUX sinon.
+     */
     public boolean payer() {
         if (this.isCotisation()) {
             System.out.println("[Membre] : " + this.getPersonne().getPrenom() + " " + this.getPersonne().getNom() + " a déjà payé.");
@@ -44,10 +88,14 @@ public class Membre {
         }
     }
 
+    /**
+     * Ajoute un vote à un arbre.
+     * @param a L'arbre de l'association considéré.
+     */
     protected void voterArbre(ArbreAssociation a) {
         try {
             if (this != null && a != null) {
-                if (a.getArbre().isEstRemarquable()) {
+                if (a.getArbre().getEstRemarquable()) {
                     throw new Exception("Arbre déjà remarquable.");
                 }
                 else {
@@ -76,6 +124,10 @@ public class Membre {
         }
         catch (Exception e) { System.out.println("[Membre] : Le vote a échoué : " + e.toString()); }
     }
+
+    /**
+     * Affiche les votes d'un membre.
+     */
     protected void afficherVote() {
         try {
             if (this != null) {
@@ -94,6 +146,11 @@ public class Membre {
             System.out.println("[Membre] : Membre introuvable.");
         }
     }
+
+    /**
+     * Retire un vote d'un membre.
+     * @param id L'id de l'arbre dont on souhaite retirer le vote.
+     */
     protected void retirerVoteArbre(int id) {
         try {
             if (this != null && id >=0) {
@@ -123,9 +180,16 @@ public class Membre {
             System.out.println("[Membre] : Le retrait du vote a échoué : " + e.toString());
         }
     }
+
+    /**
+     * Permet à un membre de changer de vote.
+     * @param a L'association considérée.
+     * @param idAncien L'id de l'arbre que l'on souhaite changer.
+     * @param idNouveau L'id de l'arbre avec lequel on souhaite échanger un vote.
+     */
     protected void remplacerVoteArbre(Association a, int idAncien, int idNouveau) {
         try {
-            if (a.getArbreById(idNouveau).getArbre().isEstRemarquable() ||
+            if (a.getArbreById(idNouveau).getArbre().getEstRemarquable() ||
                     (idAncien == idNouveau) ||
                     this.listeArbresVotes.contains(a.getArbreById(idNouveau)) ||
                     !this.listeArbresVotes.contains(a.getArbreById(idAncien)) ||
@@ -146,39 +210,72 @@ public class Membre {
         }
         catch (Exception e) { System.out.println("[Membre] : Le remplacement a échoué."); }
     }
+
+    /**
+     * Permet de retirer tous les votes d'un membre.
+     */
     protected void resetVote() {
         while(this.listeArbresVotes.size() > 0) {
             this.retirerVoteArbre(this.listeArbresVotes.getFirst().getArbre().getId());
         }
     }
 
+    /**
+     * Renvoie le nombre de votes restants d'un membre avant de devoir effacer ses anciens votes s'il vote à nouveau.
+     * @return L'attribut nbVotes du membre.
+     */
     protected int getNbVotesRestants() { return this.nbVotes; }
+
+    /**
+     * Renvoie le nombre de votes effectués par un membre qui n'entraîneront pas un effacement d'anciens votes.
+     * @return
+     */
     protected int getNbVotes() { return this.nbVotesMax - this.nbVotes; }
+
+    /**
+     * Renvoie la personne associée au membre considéré.
+     * @return La référence associée.
+     */
     protected Personne getPersonne() { return this.p; }
 
+    /**
+     * Renvoie la date d'inscription du membre sous forme d'une chaîne de caractères.
+     * @return Une chaîne de caractère au format DD/MM/AAAA.
+     */
     public String getStringDateInscription() {
         return date.dateToString(this.dateInscription);
     }
 
+    /**
+     * Renvoie une fiche d'information du membre.
+     * @return Une chaîne de caractères comportant les informations du membre.
+     */
     @Override
     public String toString() {
         return "--- Informatiions du membre ---\n" +
-               "Nom                   : " +
-               this.p.getNom() +
-               "\nPrénom                : " +
-               this.p.getPrenom() +
-               "\nDate de naissance     : " +
-               this.p.getStringDateNaissance() +
-               "\nAdresse               :\n    " +
-               this.p.getAdresse() +
-               "\n\nDate d'inscription    : " +
-               this.getStringDateInscription() +
-               "\nRôle : " +
-               ((this.president) ? "Président" : "Membre") +
-               "\nPaiement Annuel       : " +
-               ((this.cotisation) ? "Effectué" : "En attente");
+                "Nom                   : " +
+                this.p.getNom() +
+                "\nPrénom                : " +
+                this.p.getPrenom() +
+                "\nDate de naissance     : " +
+                this.p.getStringDateNaissance() +
+                "\nAdresse               :\n    " +
+                this.p.getAdresse() +
+                "\n\nDate d'inscription    : " +
+                this.getStringDateInscription() +
+                "\nRôle : " +
+                ((this.president) ? "Président" : "Membre") +
+                "\nPaiement Annuel       : " +
+                ((this.cotisation) ? "Effectué" : "En attente");
     }
 
+    // Constructeur
+
+    /**
+     * Initialise un membre à partir d'une personne et d'une date d'inscription.
+     * @param p La personne considérée.
+     * @param dateInscription Un tableau d'entiers comportant une date valide.
+     */
     protected Membre(Personne p, int[] dateInscription) {
         this.p = p;
         this.dateInscription = dateInscription;
