9f1ed164898f2c33d45a0eeb614a5d3f4eadea33
G-rnd <76098938+G-rnd@users.noreply.github.com>
2021-05-08 19:08:26 +0200

    Update ArbreAssociation.java

--- application/ArbreAssociation.java
+++ application/ArbreAssociation.java
@@ -2,7 +2,11 @@ package application;
 
 import java.util.LinkedList;
 
+/**
+ * Arbre de l'association.
+ */
 public class ArbreAssociation {
+    // Attributs
     private Arbre arbre;
 
     private int[] dateDerniereVisite = null;
@@ -11,9 +15,20 @@ public class ArbreAssociation {
     // TODO à adapter en LinkedList<CR>
     protected LinkedList<String> listeCR = null;
 
+    // Méthodes
+
+    /**
+     * Renvoie la date de classification en arbre remarquable.
+     * @return Une copie de l'attribut dateRemarquable de l'arbre.
+     */
     protected int[] getDateRemarquable() {
         return this.arbre.getDateRemarquable();
     }
+
+    /**
+     * Modifie la date de la dernière visite de l'arbre.
+     * @param d Tableau d'entier comportant une date.
+     */
     protected void setDateDerniereVisite(int[] d) {
         try {
             if (date.estValide(d)) {
@@ -28,18 +43,45 @@ public class ArbreAssociation {
         }
     }
 
+    /**
+     * Renvoie ne nombre de votes de l'arbre.
+     * @return L'attribut nbVotes de l'arbre de l'association.
+     */
     protected int getNbVotes() { return this.nbVotes; }
+
+    /**
+     * Ajoute un vote à l'arbre de l'association.
+     */
     protected void addNbVotes() { this.nbVotes++; }
+
+    /**
+     * Retire un vote à l'arbre de l'association.
+     */
     protected void removeNbVotes() { this.nbVotes--; }
+
+    /**
+     * Renvoie l'arbre associé à l'arbre de l'association considéré.
+     * @return L'arbre associé.
+     */
     protected Arbre getArbre() { return this.arbre; }
 
-    //déclarer un arbre de l'association
-    public ArbreAssociation(Arbre a) {
-        this.arbre = a;
-    }
-
     // TODO masquer = remplacer celui qui a écrit + celui qui a organisé par << personne ayant quitté l'association >>.
+
+    /**
+     * Efface toute mention écrite du membre des comptes-rendus de l'arbre de l'association.
+     * @param m Le membre considéré.
+     */
     protected void masquer(Membre m) {
 
+    }
+
+    // Constructeur
+
+    /**
+     * Crée un arbre de l'association à partir d'un arbre.
+     * @param a L'arbre associé.
+     */
+    public ArbreAssociation(Arbre a) {
+        this.arbre = a;
     }
 }
