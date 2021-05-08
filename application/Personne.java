10f718b707859e718e6f2b9b279c503ca71bc4e0
G-rnd <76098938+G-rnd@users.noreply.github.com>
2021-05-08 19:10:09 +0200

    Update Personne.java

--- application/Personne.java
+++ application/Personne.java
@@ -1,28 +1,62 @@
 package application;
 
+/**
+ * Personne.
+ */
 public class Personne {
+    // Attributs
     private String nom;
     private String prenom;
     private int[] dateNaissance;
     private String adresse;
 
+    //Méthodes
+
+    /**
+     * Renvoie le nom de la personne.
+     * @return L'attribut nom de la personne.
+     */
     public String getNom() {
         return this.nom;
     }
+
+    /**
+     * Renvoie le prénom de la personne.
+     * @return L'attribut prenom de la personne.
+     */
     public String getPrenom() {
         return this.prenom;
     }
+
+    /**
+     * Renvoie la date de naissance de la personne.
+     * @return Renvoie une chaîne de caractère correspondant à la date de naissance de la personne au format DD/MM/AAAA.
+     */
     public String getStringDateNaissance() {
         return date.dateToString(this.dateNaissance);
     }
+
+    /**
+     * Renvoie une copie de la date de naissance de la personne.
+     * @return L'attribut dateNaissance copié.
+     */
     public int[] getArrayDateNaissance() {
         return this.dateNaissance.clone();
     }
+
+    /**
+     * Renvoie l'adresse de la personne.
+     * @return L'attribut adresse de la personne.
+     */
     public String getAdresse() {
         return this.adresse;
     }
 
     @Override
+
+    /**
+     * Renvoie un booléen à VRAI si l'objet passé en paramètre est la même personne.
+     */
     public boolean equals(Object obj) {
         if (this == obj) return true;
         if (obj == null) { return false; }
@@ -35,20 +69,30 @@ public class Personne {
     }
 
     @Override
+    /**
+     * Renvoie une fiche d'information de la personne.
+     * @return Une chaîne de caractères comportant les informations de la personne.
+     */
     public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append("--- Informatiions de la personne ---\n");
-        sb.append("Nom                   : ");
-        sb.append(this.getNom());
-        sb.append("\nPrénom                : ");
-        sb.append(this.getPrenom());
-        sb.append("\nDate de naissance     : ");
-        sb.append(this.getStringDateNaissance());
-        sb.append("\nAdresse               :\n    ");
-        sb.append(this.getAdresse());
-        return sb.toString();
+        return "--- Informations de la personne ---\n" +
+               "Nom                   : " +
+               this.getNom() +
+               "\nPrénom                : " +
+               this.getPrenom() +
+               "\nDate de naissance     : " +
+               this.getStringDateNaissance() +
+               "\nAdresse               :\n    " +
+               this.getAdresse();
     }
 
+    // Constructeur
+    /**
+     * Initialise une personne à partir d'un nom, d'un prénom, d'une date de naissance et une adresse.
+     * @param nom Le nom de la personne.
+     * @param prenom Le prénom de la personne.
+     * @param dateNaissance Un tableau d'entiers comportant le jour, le mois et l'année de naissance de la personne.
+     * @param adresse L'adresse de la personne.
+     */
     public Personne(String nom, String prenom, int[] dateNaissance, String adresse) {
         try {
             if (date.estValide(dateNaissance)) {
