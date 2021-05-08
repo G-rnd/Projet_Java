3c7bd2bbd7de79b7689710e1b5c961c94e7c43e5
G-rnd <76098938+G-rnd@users.noreply.github.com>
2021-05-08 19:10:54 +0200

    Update Test.java

--- application/Test.java
+++ application/Test.java
@@ -1,86 +1,79 @@
 package application;
 
+import java.util.LinkedList;
+
 /**
- * Date.
+ * Classe de test.
  */
-public class date {
+public class Test {
     /**
-     * Renvoie un tableau d'entiers à partir d'une chaîne de caractères.
-     * @param s Une chaîne de caractères dont la date associée est au format DD/MM/AAAA.
-     * @return Un tableau contenant le jour, le mois et l'année si la date est valide, un tableau vide de taille 3 sinon.
+     * Ensemble des tests réalisés sur une association.
+     * @param a L'association associée.
      */
-    public static int[] stringToDate(String s) {
-        int[] date = new int[3];
+    public static void main(Association a) {
+        //a.afficherMembres();
+
+        a.inscrire(new Personne("a","a",new int[]{1,1,2000},"a"),
+                new int[] {1,1,2000});
+        a.inscrire(new Personne("b","b",new int[]{1,1,2000},"b"),
+                new int[] {1,1,2000});
+        Membre m1 = a.getMembreByPersonne("a","a","1/1/2000","a");
+        Membre m2 = a.getMembreByPersonne("b","b","1/1/2000","b");
+
+
+        a.nommerPresident(m1);
+        a.nommerPresident(m2);
+
+        // a.afficherMembres();
+
+        //a.desinscriptionMembre(m2);
+        // a.afficherMembres();
+
+        m2.voterArbre(a.getArbreById(100));
+        m1.voterArbre(a.getArbreById(1));
+        m1.voterArbre(a.getArbreById(2));
+        m1.voterArbre(a.getArbreById(3));
+        m1.voterArbre(a.getArbreById(3));
+
+        m1.voterArbre(a.getArbreById(4));
+        m1.voterArbre(a.getArbreById(5));
+        m1.voterArbre(a.getArbreById(6));
+        m1.voterArbre(a.getArbreById(-1));
+        m1.remplacerVoteArbre(a, 3, 100);
+  /*
+        System.out.println("### Normalement pareils");
+        a.getMembreByPersonne(m1.getPersonne().getNom(), m1.getPersonne().getPrenom(),
+                    m1.getPersonne().getStringDateNaissance(), m1.getPersonne().getAdresse()).afficherVote();
+        m1.afficherVote();
+        System.out.println("###");
+*/
+        m1.afficherVote();
+        m1.retirerVoteArbre(2);
+        System.out.println("-----");
         try {
-            String[] tab = s.split("/");
-            for(int i = 0; i < 3; i++) {
-                date[i] = Integer.parseInt(tab[i]);
+            LinkedList<Arbre> aL = a.proposerListeArbre();
+            for(Arbre arbre : aL) {
+                System.out.println(arbre.getId());
             }
         }
-        catch (Exception ignored) {}
-        return date;
-    }
+        catch (Exception e) {System.out.println(e.toString());}
 
-    /**
-     * Renvoie une chaîne de caractères à partir d'un tableau d'entiers.
-     * @param date Un tableau contenant le jour, le mois et l'année de la date considéréeL
-     * @return Une chaîne de caractère comportant une date au format DD/MM/AAAA si elle est valide, vide sinon.
-     */
-    public static String dateToString(int[] date) {
-        if (estValide(date)) {
-            return date[0] + "/" + date[1] + "/" + date[2];
+        /*
+        System.out.println("-----");
+        for(int i = 1; i < 10; i++) {
+            try {
+                System.out.println(a.getArbreById(i).getNbVotes());
+            } catch (Exception e) { System.out.println(e.toString());}
         }
-        return "";
-    }
+        */
+        System.out.println("---");
 
-    /**
-     * Permet de savoir si deux dates sont égales.
-     * @param d1 Un tableau d'entiers comportant la première date.
-     * @param d2 Un tableau d'entiers comportant la seconde date.
-     * @return Un booléen à VRAI si les dates sont les mêmes, à FAUX sinon.
-     */
-    public static boolean datesEgales(int[] d1, int[] d2) {
-        if (estValide(d1) && estValide(d2)) {
-            return  (d1[0] == d2[0]) && (d1[1] == d2[1]) && (d1[2] == d2[2]);
-        }
-        return false;
-    }
+        Personne p = new Personne("R", "G", date.stringToDate("5/9/2000"), "80 ch rb");
+        a.inscrire(p,date.stringToDate("9/5/2021"));
+        a.desinscriptionMembre(a.getMembreByPersonne("R","G","5/9/2000","80 ch rb"));
 
-    // TODO peut être faire le truc des mois à 30/31 et fév 28/29
-    /**
-     * Permet de savoir si une date est valide.
-     * @param date Un tableau d'entiers comportant la date considérée.
-     * @return Un booléen à VRAI si la date existe, à FAUX sinon.
-     */
-    public static boolean estValide(int[] date) {
-        try {
-            StringBuilder error = new StringBuilder();
-            boolean res = true;
-            if (date.length != 3) {
-                error.append("Taille incorrecte");
-                res = false;
-            }
-            if (date[0] <= 0 || date[0] > 31) {
-                if (!res) { error.append(", "); }
-                error.append("Jour incorrect");
-                res = false;
-            }
-            if (date[1] <= 0 || date[1] > 12) {
-                if (!res) { error.append(", "); }
-                error.append("Mois incorrect");
-                res = false;
-            }
-            if (date[2] <= 1900) {
-                if (!res) { error.append(", "); }
-                error.append("Année incorrecte");
-                res = false;
-            }
-            if (!res) { throw new ExceptionInInitializerError(error.toString()); }
-            else { return true; }
-        }
-        catch (ExceptionInInitializerError e) {
-            System.out.println("Erreur : Date saisie incorrecte : " + e.toString() + ".");
-            return false;
-        }
+        System.out.println(p.toString());
+
+        System.out.println("Fin.");
     }
 }
