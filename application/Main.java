dc8af2bced3bafde27d8cbd1c7d6bd7afbb2f47b
G-rnd <76098938+G-rnd@users.noreply.github.com>
2021-05-30 00:31:15 +0200

    Update Main.java

--- application/Main.java
+++ application/Main.java
@@ -8,52 +8,57 @@ import java.util.ArrayList;
 /**
  * Main.
  */
-public class Main
-{
+public class Main {
     /**
      * Fonction Principale du projet.
+     *
      * @param args La liste des arguments : [0]: La source du fichier csv.
-     *                                      [1]: Affiche le test avec l'argument test.
-     *                                      [2]: Affiche seulement le test avec l'argument only.
+     *             [1]: Affiche le test avec l'argument test.
+     *             [2]: Affiche seulement le test avec l'argument only.
      */
     public static void main(String[] args) {
         // techniquement déclarer dans espaces verts
         ArrayList<Arbre> listeArbres = null;
-        if(args.length > 0)
-        {
+        if (args.length > 0) {
             File tempFile = new File(args[0]);
-            if(tempFile.exists())
-            {
+            if (tempFile.exists()) {
                 System.out.println("[Main] : Lecture du fichier " + args[0] + " ...");
 
                 //We start by reading the CSV file
                 listeArbres = FileReader.getDataFromCSVFile(args[0]);
 
                 System.out.println("[Main] : Fin du fichier " + args[0] + ".");
+            } else {
+                System.out.println("[Main] Aucun fichier trouvé.");
             }
-            else { System.out.println("[Main] Aucun fichier trouvé."); }
+        } else {
+            System.out.println("[Main] Aucun argument trouvé.");
         }
-        else { System.out.println("[Main] Aucun argument trouvé."); }
 
-        if(listeArbres != null) {
-            Association AssociationArbre = new Association(listeArbres);
+        if (listeArbres != null) {
+            Municipalite municipalite = new Municipalite("Mairie de Paris", listeArbres);
+            Association association = new Association(listeArbres, 20, 40);
+            municipalite.ajouterEntiteNotifiable(association);
+
+            ArrayList<Donateur> listeDonateurs = new ArrayList<>();
+            listeDonateurs.add(municipalite);
+
             // l'argument 2 est pour le debug
             try {
                 if (args[1].equals("test")) {
-                    Test.main(AssociationArbre);
+                    Test.main(association);
                 }
-            }
-            catch (Exception e) { /* pas de debug. */ }
+            } catch (Exception e) { /* pas de debug. */ }
             try {
                 if (!args[2].equals("only")) {
                     // lancement de l'application.
-                    Application.main(AssociationArbre);
+                    Application.main(association, municipalite, listeDonateurs);
                 }
+            } catch (Exception e) {
+                Application.main(association, municipalite, listeDonateurs);
             }
-            catch (Exception e) {
-                Application.main(AssociationArbre);
-            }
+        } else {
+            System.out.println("[Main] : Aucun Arbre n'a été lu.");
         }
-        else { System.out.println("[Main] : Aucun Arbre n'a été lu."); }
     }
 }
