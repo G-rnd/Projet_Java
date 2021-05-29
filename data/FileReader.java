e7b7d10479610944dfbe36e845cfabafdd137b52
G-rnd <76098938+G-rnd@users.noreply.github.com>
2021-05-30 00:26:54 +0200

    Update FileReader.java

--- data/FileReader.java
+++ data/FileReader.java
@@ -9,10 +9,8 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 
-public class FileReader
-{
-    public static ArrayList<Arbre> getDataFromCSVFile(String csvFilePath)
-    {
+public class FileReader {
+    public static ArrayList<Arbre> getDataFromCSVFile(String csvFilePath) {
         String line;
         String[] data;
         String separator = ";(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)";
@@ -31,23 +29,20 @@ public class FileReader
         Float[] geographicalPoint2D = new Float[2];
 
         boolean valide = true;
-        try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(csvFilePath), StandardCharsets.ISO_8859_1))
-        {
+        try (BufferedReader bufferedReader = Files.newBufferedReader(Paths.get(csvFilePath), StandardCharsets.ISO_8859_1)) {
             //Read the first line
             line = bufferedReader.readLine();
 
             //Get data from the line
             data = line.split(separator, -1);
 
-            if(data.length != 17)
-            {
+            if (data.length != 17) {
                 System.out.println("[FileReader] The file at " + csvFilePath + " does not contain the right number of columns.");
                 return listeArbre;
             }
 
             //Read the file line by line
-            while ((line = bufferedReader.readLine()) != null)
-            {
+            while ((line = bufferedReader.readLine()) != null) {
                 //Get data from the line
                 data = line.split(separator, -1);
                 //Sort data
@@ -55,89 +50,65 @@ public class FileReader
 
                 valide = true;
                 //Get the base ID
-                try
-                {
+                try {
                     idBase = Integer.parseInt(data[0]);
-                }
-                catch (Exception exception)
-                {
+                } catch (Exception exception) {
                     idBase = null;
                     valide = false;
                 }
 
                 //Get the address
-                try
-                {
+                try {
                     adresse = data[6];
-                }
-                catch (Exception exception)
-                {
+                } catch (Exception exception) {
                     adresse = "";
                     valide = false;
                 }
 
                 //Get the French name
-                try
-                {
+                try {
                     libelleFrancais = data[8];
-                }
-                catch (Exception exception)
-                {
+                } catch (Exception exception) {
                     libelleFrancais = "";
                     valide = false;
                 }
 
                 //Get the genus
-                try
-                {
+                try {
                     genre = data[9];
-                }
-                catch (Exception exception)
-                {
+                } catch (Exception exception) {
                     genre = "";
                     valide = false;
                 }
 
                 //Get the specie
-                try
-                {
+                try {
                     espece = data[10];
-                }
-                catch (Exception exception)
-                {
+                } catch (Exception exception) {
                     espece = "";
                     valide = false;
                 }
 
                 //Get the circumference (cm)
-                try
-                {
+                try {
                     circonferenceEnCm = Integer.parseInt(data[12]);
-                }
-                catch (Exception exception)
-                {
+                } catch (Exception exception) {
                     circonferenceEnCm = null;
                     valide = false;
                 }
 
                 //Get the height (m)
-                try
-                {
+                try {
                     hauteurEnM = Integer.parseInt(data[13]);
-                }
-                catch (Exception exception)
-                {
+                } catch (Exception exception) {
                     hauteurEnM = null;
                     valide = false;
                 }
 
                 //Get the development state
-                try
-                {
+                try {
                     stadeDeveloppement = data[14];
-                }
-                catch (Exception exception)
-                {
+                } catch (Exception exception) {
                     stadeDeveloppement = "";
                     valide = false;
                 }
@@ -145,9 +116,7 @@ public class FileReader
                 //Get whether the tree is remarquable or not
                 try {
                     remarquable = data[15].equals("OUI") || data[15].equals("oui");
-                }
-                catch (Exception exception)
-                {
+                } catch (Exception exception) {
                     remarquable = false;
                     valide = false;
                 }
@@ -156,26 +125,19 @@ public class FileReader
                 try {
 
                     data = data[16].split(",", -1);
-                    try
-                    {
+                    try {
                         geographicalPoint2D[0] = Float.parseFloat(data[0]);
-                    }
-                    catch (Exception exception)
-                    {
+                    } catch (Exception exception) {
                         geographicalPoint2D[0] = null;
                         valide = false;
                     }
-                    try
-                    {
+                    try {
                         geographicalPoint2D[1] = Float.parseFloat(data[1]);
-                    }
-                    catch (Exception exception)
-                    {
+                    } catch (Exception exception) {
                         geographicalPoint2D[1] = null;
                         valide = false;
                     }
-                }
-                catch (Exception exception) {
+                } catch (Exception exception) {
                     geographicalPoint2D = new Float[]{null, null};
                     valide = false;
                 }
@@ -186,9 +148,7 @@ public class FileReader
                             adresse, new double[]{geographicalPoint2D[0], geographicalPoint2D[1]}, remarquable));
                 }
             }
-        }
-        catch (IOException exception)
-        {
+        } catch (IOException exception) {
             System.err.println(exception);
         }
         return listeArbre;
